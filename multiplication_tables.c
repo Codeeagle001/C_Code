7e0a28a4db68824cf833dfc5d9424d5fa4b4435a
lxt <ljun0122@163.com>
2023-03-02 11:20:49 +0000

    3月2日
    Signed-off-by: lxt <ljun0122@163.com>

--- multiplication_tables.c
+++ multiplication_tables.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
-#define LOW 1
-#define HIG 9
+// #define LOW 1
+// #define HIG 9
 int main()
 {
-    int i;
-    int j;
-    for(i = LOW; i <= HIG; i++){
-        for(j = LOW; j <= (i + 1); j++){
-            printf("%d * %d = %2d", j, i, (i * j));
-            printf(" ");
-        }
-        printf("\n");
+    // for循环嵌套打印九九乘法表
+    // int i;
+    // int j;
+    // for(i = LOW; i <= HIG; i++){
+    //     for(j = LOW; j <= (i + 1); j++){
+    //         printf("%d * %d = %2d", j, i, (i * j));
+    //         printf(" ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // n的阶乘
+    // int i = 0, n = 0;
+    // int sum = 1;
+    // scanf("%d", &n);
+    // for (i = 1; i <= n; i++){
+    //     sum *= i;
+    // }
+    // printf("%d\n", sum);
+    
+
+    // 1! + 2! + 3! + ... + 10!
+    int i = 0, n = 0, num = 0;
+    int sum = 1;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++){
+        sum *= i;
+        num += sum;
     }
+    printf("%d\n", num);
+    
     return 0;
+
+
 }
